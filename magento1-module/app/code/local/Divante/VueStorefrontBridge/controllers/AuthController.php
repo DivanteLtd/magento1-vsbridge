7fc3c850fc926d4b8886b32284574502610eb593
Tobias Dalhof <t.dalhof@alfa-direkt.de>
2019-03-06 16:00:06 +0100

    Refactored AuthController

--- magento1-module/app/code/local/Divante/VueStorefrontBridge/controllers/AuthController.php
+++ magento1-module/app/code/local/Divante/VueStorefrontBridge/controllers/AuthController.php
@@ -1,6 +1,6 @@
 <?php
-require_once('AbstractController.php');
-require_once(__DIR__ . '/../helpers/JWT.php');
+require_once 'AbstractController.php';
+require_once __DIR__ . '/../helpers/JWT.php';
 
 /**
  * Divante VueStorefrontBridge AuthController Class
@@ -23,29 +23,23 @@ class Divante_VueStorefrontBridge_AuthController extends Divante_VueStorefrontBr
     {
         if ($this->getRequest()->getMethod() !== 'POST') {
             return $this->_result(500, 'Only POST method allowed');
-        } else {
-            $request = $this->_getJsonBody();
-
-            if (!$request) {
-                return $this->_result(500, 'No JSON object found in the request body');
-            } else {
-                if (!$request->username || !$request->password) {
-                    return $this->_result(500, 'No username or password given!');
-                } else {
-                    $session   = Mage::getSingleton('admin/session');
-                    $secretKey = $this->getSecretKey();
-                    $user      = $session->login($request->username, $request->password);
-
-                    if ($user->getId()) {
-                        return $this->_result(200, JWT::encode(['id' => $user->getId()], $secretKey));
-                    } else {
-                        return $this->_result(
-                            500,
-                            'You did not sign in correctly or your account is temporarily disabled.'
-                        );
-                    }
-                }
-            }
         }
+
+        $request = $this->_getJsonBody();
+        if (!$request) {
+            return $this->_result(500, 'No JSON object found in the request body');
+        }
+        if (!$request->username || !$request->password) {
+            return $this->_result(500, 'No username or password given!');
+        }
+
+        $session = Mage::getSingleton('admin/session');
+        $secretKey = $this->getSecretKey();
+        $user = $session->login($request->username, $request->password);
+
+        if (empty($user->getId())) {
+            return $this->_result(500, 'You did not sign in correctly or your account is temporarily disabled.');
+        }
+        return $this->_result(200, JWT::encode(['id' => $user->getId()], $secretKey));
     }
 }
