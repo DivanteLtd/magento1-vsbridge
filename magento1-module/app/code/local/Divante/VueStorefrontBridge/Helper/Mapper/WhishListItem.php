bcc11a1ef913652682ec57ec74bb50e44600b8e5
Mathias Arlaud <mathias.arlaud@gmail.com>
2019-04-23 15:55:00 +0200

    Update whishlist

---
+++ magento1-module/app/code/local/Divante/VueStorefrontBridge/Helper/Mapper/WhishListItem.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Divante VueStorefrontBridge WhishListItemMapper Class
+ *
+ * @category    Divante
+ * @package     VueStorefrontBridge
+ * @author      Mathias Arlaud <marlaud@sutunam.com>
+ * @copyright   Copyright (C) 2019
+ * @license     MIT License
+ */
+class Divante_VueStorefrontBridge_Helper_Mapper_WhishListItem extends Divante_VueStorefrontBridge_Helper_Mapper_Abstract
+{
+}
